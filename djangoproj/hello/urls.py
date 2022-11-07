2a51b6c7b14e736a354f4b857a4b24ecb21f43b8
PreciousPrado <preciousprado23@gmail.com>
2022-11-07 22:13:18 +0800

    new commit

--- djangoproj/hello/urls.py
+++ djangoproj/hello/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
 path('',views.index, name='index'),
-path('test',views.index, name='test'),
-path('test2',views.index, name='test2'),
+path('test',views.test, name='test'),
+path('test2',views.test2, name='test2')
 ]
