38602d2decf3371226ed6a02b4b5f3593bf5c276
PreciousPrado <preciousprado23@gmail.com>
2022-11-07 20:47:26 +0800

    new commit

--- djangoproj/hello/views.py
+++ djangoproj/hello/views.py
@@ -6,3 +6,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, BSIT ")
+
+def test(request):
+    return HttpResponse("<a href='#'>Link<a/>")
+
+def test2(request):
+    return HttpResponse("<h1>Test 2<h1/>")
